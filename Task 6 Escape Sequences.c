46673d0cfe56780d27af94ccf69e95a09fdbf317
MuhammadZubair6516 <muhammadzubair6879@gmail.com>
2026-02-06 22:09:04 +0500

    Add files via upload

---
+++ Task 6 Escape Sequences.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() 
+{
+    printf("Name:   \tZubair Arain\n");
+    printf("Roll No:\t25K6516\n");
+    printf("Section:\tBSCE-A\n");
+    return 0;
+}
---
+++ Task 8 User Input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+    int age ;
+    float height ;
+    char grade ;
+
+    printf("Age:");
+    scanf("%d", &age);
+    printf("Height:");
+     scanf("%f", &height);
+    printf("Grade: ");
+     scanf(" %c", &grade);
+
+    return 0;
+}
